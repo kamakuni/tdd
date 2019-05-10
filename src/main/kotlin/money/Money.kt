a75313e1a38b3b53c5933a4e5879227735ab32a9
kamakuni <superradio86@gmail.com>
2019-05-10 21:59:14 +0900

    update main file name

--- src/main/kotlin/money/Money.kt
+++ src/main/kotlin/money/Money.kt
@@ -3,18 +3,11 @@
  */
 package money
 
-class App {
-    val greeting: String
-        get() {
-            return "Hello world."
-        }
-}
-
 class Doller(val amount: Int){ // TODO limit scope
     fun times(multiplier: Int): Doller = Doller(amount * multiplier)
     override fun equals(other: Any?) = (other is Doller) && amount == other.amount
 }
 
 fun main(args: Array<String>) {
-    println(App().greeting)
+    println("Nothing")
 }
