f995773d8a6efbd332c67a3ed826349f900f15c2
kamakuni <superradio86@gmail.com>
2019-05-11 21:03:10 +0900

    move times methods from sub class to super class

--- src/main/kotlin/money/Money.kt
+++ src/main/kotlin/money/Money.kt
@@ -3,16 +3,21 @@
  */
 package money
 
-open class Money(open val amount: Int) {
+abstract class Money(open val amount: Int) {
+    companion object {
+        fun dollar(amount: Int) = Dollar(amount)
+        fun franc(amount: Int) = Franc(amount)
+    }
     override fun equals(other: Any?) = (other is Money) && amount == other.amount && this::class.simpleName == other::class.simpleName
+    abstract fun times(multiplier: Int): Money
 }
 
 class Dollar(override val amount: Int): Money(amount) {
-    fun times(multiplier: Int): Dollar = Dollar(amount * multiplier)
+    override fun times(multiplier: Int): Money = Dollar(amount * multiplier)
 }
 
 class Franc(override val amount: Int): Money(amount) {
-    fun times(multiplier: Int): Franc = Franc(amount * multiplier)
+    override fun times(multiplier: Int): Money = Franc(amount * multiplier)
 }
 
 fun main(args: Array<String>) {
