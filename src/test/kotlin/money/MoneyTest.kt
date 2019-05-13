0dcc0ca63bfca6a5a7e2db617fc0016c1c1bfc4f
kamakuni <superradio86@gmail.com>
2019-05-13 23:40:33 +0900

    remove unnecessary codes

--- src/test/kotlin/money/MoneyTest.kt
+++ src/test/kotlin/money/MoneyTest.kt
@@ -19,15 +19,7 @@ class MoneyTest {
     @Test fun testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)))
         assertFalse(Money.dollar(5).equals(Money.dollar(6)))
-        assertTrue(Money.franc(5).equals(Money.franc(5)))
-        assertFalse(Money.franc(5).equals(Money.franc(6)))
         assertFalse(Money.franc(5).equals(Money.dollar(5)))
-    }
-
-    @Test fun testFrancMultiplication() {
-        val five = Money.franc(5)
-        assertEquals(Money.franc(10), five.times(2))
-        assertEquals(Money.franc(15), five.times(3))
     }
 
     @Test fun testCurrency() {
