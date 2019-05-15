d376d9589162c04d9a5d1e86441eb458fb38fc47
kamakuni <superradio86@gmail.com>
2019-05-15 23:45:35 +0900

    add Bank class and Expression interface

--- src/test/kotlin/money/MoneyTest.kt
+++ src/test/kotlin/money/MoneyTest.kt
@@ -27,8 +27,11 @@ class MoneyTest {
         assertEquals("CHF", Money.franc(1).currency())
     }
 
-    @Test fun testAddtion() {
-        val sum = Money.dollar(5).plus(Money.dollar(5))
-        assertEquals(Money.dollar(10),sum)
+    @Test fun testSimpleAddition() {
+        val five = Money.dollar(5)
+        val sum = five.plus(five)
+        val bank = Bank()
+        val reduced = bank.reduce(sum, "USD")
+        assertEquals(Money.dollar(10), reduced)
     }
 }
